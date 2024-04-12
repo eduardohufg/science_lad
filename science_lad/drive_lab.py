8ed6be47fcd0b9f6ccac45c3294a1b6cab00bb2c
eduardohufg <eduardochavezmartin10@gmail.com>
2024-04-12 01:03:57 -0600

    first commit

---
+++ science_lad/drive_lab.py
@@ -0,0 +1,78 @@
+#Declare libraries
+import rclpy
+from .submodules.lx16a import *
+#from lx16a import *
+from math import *
+from std_msgs.msg import *
+from numpy import*
+from rclpy.node import Node
+import serial
+
+class DriveScienceListener(Node):
+    
+    def __init__(self):
+        # Init the node
+        super().__init__('listener_drive_lab')
+        self.subscriber_elevator = self.create_subscription(Int8,"science/elevator",self.callbackelevator,10)
+        self.subscriber_elevator
+        self.subscriber_servo1 = self.create_subscription(Int8,"science/servos", self.callbackservo1,10)
+        self.subscriber_servo1
+        self.subscriber_servo2 = self.create_subscription(Int8,"science/servos", self.callbackservo2,10)
+        self.subscriber_servo2
+        try:
+            LX16A.initialize("/dev/SERVO_CONTROLLER")
+        except Exception as e:
+            print(e)
+
+        try: 
+            self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout= 1)
+        except Exception as e:
+            print(e)
+
+        self.servo1 = LX16A(1)
+        self.servo3 = LX16A(3)
+        self.servo1.servoMode()
+        self.servo3.servoMode()
+
+        self.servo1_data = None
+        self.servo2_data = None
+        self.elevator_data = None
+
+        self.timer = self.create_timer(0.05, self.main)
+
+    def callbackservo1(self,data):
+        self.servo1_data = Int8(data.data)
+
+    def callbackservo2(self,data):
+        self.servo2_data = Int8(data.data)
+    
+    def callbackelevator(self,data):
+        self.elevator_data = Int8(data.data)
+
+
+    def main(self):
+        print("entre")
+
+        self.servo1.moveTimeWrite(int(self.servo1_data))
+        self.servo3.moveTimeWrite(int(self.servo2_data))
+
+        if(self.elevator_data == 1):
+            self.ser.write(('1\n').encode())
+        elif(self.elevator_data == -1):
+            self.ser.write(('0\n').encode())
+        else:
+            self.ser.write(('2\n').encode())
+
+
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    listener=DriveScienceListener()
+    rclpy.spin(listener)
+    listener.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'science_lad'
+submodules = 'science_lad/submodules'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name, submodules],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eduardo',
+    maintainer_email='eduardo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drive_lab = science_lad.drive_lab:main'
+        ],
+    },
+)
