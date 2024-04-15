3d6c096fbe8a404d65cf8cd1f0149d44485e6066
eduardohufg <eduardochavezmartin10@gmail.com>
2024-04-15 01:42:30 -0600

    callback corrections

--- science_lad/drive_lab.py
+++ science_lad/drive_lab.py
@@ -15,12 +15,12 @@ class DriveScienceListener(Node):
         super().__init__('listener_drive_lab')
         self.subscriber_elevator = self.create_subscription(Int8,"science/elevator",self.callbackelevator,10)
         self.subscriber_elevator
-        self.subscriber_servo1 = self.create_subscription(Int8,"science/servos", self.callbackservo1,10)
+        self.subscriber_servo1 = self.create_subscription(Int8,"science/servos1", self.callbackservo1,10)
         self.subscriber_servo1
-        self.subscriber_servo2 = self.create_subscription(Int8,"science/servos", self.callbackservo2,10)
+        self.subscriber_servo2 = self.create_subscription(Int8,"science/servos2", self.callbackservo2,10)
         self.subscriber_servo2
         try:
-            LX16A.initialize("/dev/SERVO_CONTROLLER")
+            LX16A.initialize("/dev/ttyUSB1")
         except Exception as e:
             print(e)
 
@@ -30,9 +30,9 @@ class DriveScienceListener(Node):
             print(e)
 
         self.servo1 = LX16A(1)
-        self.servo3 = LX16A(3)
+        #self.servo3 = LX16A(3)
         self.servo1.servoMode()
-        self.servo3.servoMode()
+        #self.servo3.servoMode()
 
         self.servo1_data = None
         self.servo2_data = None
@@ -41,13 +41,13 @@ class DriveScienceListener(Node):
         self.timer = self.create_timer(0.05, self.main)
 
     def callbackservo1(self,data):
-        self.servo1_data = Int8(data.data)
+        self.servo1_data = int(data.data)
 
     def callbackservo2(self,data):
-        self.servo2_data = Int8(data.data)
+        self.servo2_data = int(data.data)
     
     def callbackelevator(self,data):
-        self.elevator_data = Int8(data.data)
+        self.elevator_data = int(data.data)
 
 
     def main(self):
@@ -60,12 +60,12 @@ class DriveScienceListener(Node):
         elif(self.servo1_data == 1):
             self.servo1.moveTimeWrite(185)
 
-        if(self.servo2_data == -1):
-            self.servo3.moveTimeWrite(0)
-        elif(self.servo2_data == 0):
-            self.servo3.moveTimeWrite(95)
-        elif(self.servo2_data == 1):
-            self.servo3.moveTimeWrite(185)
+        #if(self.servo2_data == -1):
+            #self.servo3.moveTimeWrite(0)
+        #elif(self.servo2_data == 0):
+            #self.servo3.moveTimeWrite(95)
+        #elif(self.servo2_data == 1):
+            #self.servo3.moveTimeWrite(185)
 
         #self.servo1.moveTimeWrite(int(self.servo1_data))
         #self.servo3.moveTimeWrite(int(self.servo2_data))
@@ -73,7 +73,8 @@ class DriveScienceListener(Node):
         if(self.elevator_data == 1):
             self.ser.write(('1\n').encode())
         elif(self.elevator_data == -1):
-            self.ser.write(('0\n').encode())
+            self.ser.write(('-1\n').encode())
+        self.elevator_data == 0
         #else:
             #self.ser.write(('2\n').encode())
 
